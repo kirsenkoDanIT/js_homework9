11846e2950a1e14494b5b62496adc5a3dcfdcfc0
kirsenko <46247205+kirsenko@users.noreply.github.com>
2019-05-24 11:10:19 +0300

    1

--- js/script.js
+++ js/script.js
@@ -1,8 +1,6 @@
-'use strict'
+"use strict";
 const tabs = document.querySelector(".tabs");
 const tabsContent = document.querySelector(".tabs-content");
-console.log(tabs);
-console.log(tabsContent);
 
 tabs.addEventListener("click", e => {
   [...tabs.children].forEach(elem => {
@@ -11,21 +9,19 @@ tabs.addEventListener("click", e => {
   e.target.classList.add("active");
 
   [...tabsContent.children].forEach(elem => {
-    elem.style.display = 'none';
+    elem.style.display = "none";
     if (elem.dataset.name === e.target.innerText) {
-      elem.style.display = 'block';
+      elem.style.display = "block";
     }
   });
 });
 
 [...tabs.children].forEach(tabsElem => {
-  if (tabsElem.classList.contains('active')) {
+  if (tabsElem.classList.contains("active")) {
     [...tabsContent.children].forEach(contentElem => {
-      contentElem.style.display = 'none';
       if (contentElem.dataset.name === tabsElem.innerText) {
-        contentElem.style.display = 'block';
+        contentElem.style.display = "block";
       }
     });
   }
 });
-
